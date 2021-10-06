050601e71569ccb4d72250dc2225ca0b18b2e181
George Blue <gblue@pivotal.io>
2021-10-06 17:17:27 +0100

    spike: upgrade to 0.13
    This is spike code - do not merge
    
    [#179537361](https://www.pivotaltracker.com/story/show/179537361)

--- terraform/rds/provision/provider.tf
+++ terraform/rds/provision/provider.tf
@@ -16,8 +16,18 @@ variable aws_access_key_id { type = string }
 variable aws_secret_access_key { type = string }
 variable region { type = string }
 
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
-  version = "~> 3.0"
   region  = var.region
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
